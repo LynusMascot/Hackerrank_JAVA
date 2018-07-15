dc1edbeaa56b12b17f86cc79badba22fee6a1a6e
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-15 12:08:21 +0530

    Update Java _Strings_Introduction.java

--- Java _Strings_Introduction.java
+++ Java _Strings_Introduction.java
@@ -10,11 +10,13 @@ public class Solution {
         String B=sc.nextLine();
         
         System.out.println(A.length()+B.length());
-        int a= B.compareTo(A);
-        if(a==1){
+        
+        if(A.compareTo(B)>0){
             System.out.println("Yes");
         }else{System.out.println("No");}
-        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
+        String a= A.substring(0,1).toUpperCase()+A.substring(1);
+        String b= B.substring(0,1).toUpperCase()+B.substring(1);
+        System.out.println(a+" "+b);
         
     }
 }
