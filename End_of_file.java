33ecd0153399b7ebfe1feced4f1307546dae85e8
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-20 08:10:16 +0530

    Create End_of_file.java

---
+++ End_of_file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+       while (sc.hasNextLine() == true) {
+    if (sc.hasNext() == true) {
+        System.out.println(i + " " + sc.nextLine());
+        i++;
+    }
+    }
+    }
+}
